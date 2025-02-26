e762472fc288b8ee60677cdf2c5f2a9f33b3376c
MEHTAB <mehtabarifahad@gmail.com>
2025-02-26 14:12:00 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import Home from './src/screens/home_screen/index'
+
+export default function App() {
+  return (
+    <SafeAreaView >
+      
+      <Home/>
+    </SafeAreaView>
+  );
+}
